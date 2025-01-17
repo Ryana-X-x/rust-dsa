6c88af187ac41b9be1fc51f7ce86ebaf9c32f814
ryanaU <aryansehgal3212@gmail.com>
2025-01-17 13:44:01 +0530

    two sum comments

--- DSA/Neetcode/Arrays&Hashing/two_sum.rs
+++ DSA/Neetcode/Arrays&Hashing/two_sum.rs
@@ -3,15 +3,15 @@ use std::collections::HashMap ;
 fn two_sum(nums:Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = HashMap::new() ;
 
-    for (i, &num) in nums.iter().enumerate() {
-        let complement = target - num ;
-        if let Some(&index) = map.get(&complement) {
-            return vec![index as i32, i as i32] ;
+    for (i, &num) in nums.iter().enumerate() {  // enumerate gives both the index and the value for num
+        let complement = target - num ;  
+        if let Some(&index) = map.get(&complement) {// checking if a value at an index in hashmap = complement
+            return vec![index as i32, i as i32] ;   // returning the index of the complement and the current value
         }
-        map.insert(num, i) ;
+        map.insert(num, i) ;    // if not found then adding the current value and it's index for future check 
     }
 
-    vec![]     
+    vec![]     // defualt case: Should not be reached if guranteed solution 
 }
 
 
