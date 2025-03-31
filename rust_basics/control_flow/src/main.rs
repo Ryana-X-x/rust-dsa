f7e64af49be8ca16afc87aa6cbe0d08eae4110f4
ryana-wsl <aryansehgal3212@gmail.com>
2025-03-31 13:12:56 +0000

    loops: while, loop, for

--- rust_basics/control_flow/src/main.rs
+++ rust_basics/control_flow/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     println!("Hello from Ryana!");
 
     test_if() ;
+    test_while();
+
+    test_loop();
+    test_for();
 }
 
 fn test_if() {
@@ -11,7 +15,8 @@ fn test_if() {
     let my_input: &mut String = &mut String::from("") ;
     std::io::stdin().read_line(my_input).unwrap() ;
 
-    let age: u8 = my_input.replace("\n", "").parse::<u8>().unwrap() ;
+    // let age: u8 = my_input.replace("\n", "").parse::<u8>().unwrap() ;
+    let age: u8 = my_input.trim().parse::<u8>().unwrap() ;  // better than replace here
     if age > age_to_drive {
         println!("Issuing driver's license") ;
         
@@ -23,4 +28,42 @@ fn test_if() {
 
     let driver_license: bool = if age >= age_to_drive { true } else { false } ;
     println!("Driver's license: {0}", driver_license) ;
+}
+
+fn test_while() {
+    let age_to_drive = 18u8  ;
+    
+    let mut current_age = 0u8 ;
+    while current_age < age_to_drive {
+        println!("Waiting..") ;
+        current_age += 1 ;
+
+        if current_age == 18 {
+            println!("Finally of age!") ;
+        }
+    }
+}
+
+fn test_loop() {
+    let mut x: i32 = 1 ;
+
+    loop {
+        println!("Hello From Ryana!") ;
+
+        if x > 5 { break } ;
+
+        x += 1 ; 
+    }
+}
+
+fn test_for() {
+    let ages = [14, 18, 23, 45, 67] ;
+    let age_to_drive = 18i32  ;
+
+    for value in ages {
+        println!("The current age is: {0}", value) ;
+        if value >= age_to_drive { println!("You can get a driver's license")} 
+        else { println!("You cannot get driver's license")} ;
+    }
+
 }
